cd5b55e2521bd3ce25932748fb1c672579fbbd6e
Najot Nafasov <nigelnafasov98@yandex.com>
2025-09-27 13:01:03 +0500

    исправление ошибок модального окна с заказами

--- src/components/order-info/order-info.tsx
+++ src/components/order-info/order-info.tsx
@@ -1,23 +1,28 @@
 import { FC, useMemo } from 'react';
 import { Preloader } from '../ui/preloader';
 import { OrderInfoUI } from '../ui/order-info';
-import { TIngredient } from '@utils-types';
+import { TIngredient, TOrder } from '@utils-types';
+import { useSelector } from '../../services/store';
+import { useParams } from 'react-router-dom';
+import { getAllOrders } from '../../services/slices/feedSlice';
+import { getIngredients } from '../../services/slices/ingredientsSlice';
+import { getAuthenticated } from '../../services/slices/userSlice';
+import { getUserOrders } from '../../services/slices/userOrderSlice';
 
 export const OrderInfo: FC = () => {
-  /** TODO: взять переменные orderData и ingredients из стора */
-  const orderData = {
-    createdAt: '',
-    ingredients: [],
-    _id: '',
-    status: '',
-    name: '',
-    updatedAt: 'string',
-    number: 0
-  };
+  const params = useParams();
+  const ingredients: TIngredient[] = useSelector(getIngredients);
+  const isAuth = useSelector(getAuthenticated);
+  const orders = useSelector(getAllOrders);
+  const userOrders = useSelector(getUserOrders);
+  let allOrders: TOrder[];
 
-  const ingredients: TIngredient[] = [];
+  !isAuth ? (allOrders = orders) : (allOrders = orders.concat(userOrders));
 
-  /* Готовим данные для отображения */
+  const orderData: TOrder | undefined = allOrders.find(
+    (i) => i.number === Number(params.number)
+  );
+
   const orderInfo = useMemo(() => {
     if (!orderData || !ingredients.length) return null;
 
